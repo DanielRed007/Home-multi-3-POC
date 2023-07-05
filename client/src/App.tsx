c68971192ac58e202ed5635e0d3001a4b7d19e34
daniel.mesu <daniel.mesu@coderio.co>
2023-07-05 23:07:02 +0100

    refactor to only dark mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,14 @@
 import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
-import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+import Button from '@mui/material/Button';
 
 function App() {
-  const theme = useTheme();
-  const colorMode = React.useContext(ColorModeContext);
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        bgcolor: 'background.default',
-        color: 'text.primary',
-        borderRadius: 1,
-        p: 3,
-      }}
-    >
-      {theme.palette.mode} mode
-      <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
-    </Box>
+    <div>
+      <Button variant="contained" disableElevation>
+        Disable elevation
+      </Button>
+    </div>
   );
 }
 
